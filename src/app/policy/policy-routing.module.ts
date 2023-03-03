64e306a98f53d5b7a7b3c120503ca059b21add07
Freddy Mande <freddoss18@gmail.com>
2023-03-02 20:59:07 -0800

    quick fixes

--- src/app/policy/policy-routing.module.ts
+++ src/app/policy/policy-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PolicyComponent } from './policy.component';
 
-const routes: Routes = [{ path: '', component: PolicyComponent }];
+const routes: Routes = [
+  { path: '', component: PolicyComponent },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PolicyRoutingModule { }
+export class PolicyRoutingModule {}
